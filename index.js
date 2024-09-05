2cf98ae8c8223e3d703ca8db83ff407d243b3898
suvrodeb <suvrodev.cse.77@gmail.com>
2024-09-05 15:28:47 +0600

    server is okay

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const express = require('express');
-const cors = require('cors');
-const we=require('./Data/We.json')
+const express = require("express");
+const cors = require("cors");
+const we = require("./Data/We.json");
 
+const app = express();
+const port = process.env.PROT || 7000;
 
-const app=express();
-const port=process.env.PROT || 7000
-
-
-require('dotenv').config()
+require("dotenv").config();
 
 //MiddleWare
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send(`Ema-John server is running on port: ${port} `);
+});
 
-app.get('/',(req,res)=>{
-    res.send(`Ema-John server is running on port: ${port} `)
-})
+app.get("/we", (req, res) => {
+  console.log("Wee All");
+  res.send(we);
+});
 
-app.get('/we',(req,res)=>{
-    console.log('Wee All');
-    res.send(we)
-})
-
-app.get('/we/:id',(req,res)=>{
-    const id=req.params.id;
-    console.log(id);
-    const targetId=we.find(w=>w.id==id)
-    res.send(targetId)
-})
-
-
-
+app.get("/we/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const targetId = we.find((w) => w.id == id);
+  res.send(targetId);
+});
 
 /////MongoDB work start
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jokwhaf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -48,7 +39,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -58,7 +49,7 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     ///Project work start
-    const productCollection=client.db('emaJohnDB').collection('products')
+    const productCollection = client.db("emaJohnDB").collection("products");
 
     // app.get('/products', async(req,res)=>{
     //     console.log('All Products');
@@ -66,52 +57,53 @@ async function run() {
     //     res.send(result)
     // })
 
-    app.get('/products', async(req,res)=>{
-        console.log(req.query);
-        const page=parseInt(req.query.page) || 2
-        const limit=parseInt(req.query.limit) || 10
-        const skip=page*limit
-        console.log(page);
-        console.log(limit);
-        console.log('All Products');
-        const result= await productCollection.find().skip(skip).limit(limit).toArray()
-        res.send(result)
-    })
+    app.get("/products", async (req, res) => {
+      console.log(req.query);
+      const page = parseInt(req.query.page) || 2;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+      console.log(page);
+      console.log(limit);
+      console.log("All Products");
+      const result = await productCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+      res.send(result);
+    });
 
-    app.get('/totalproducts',async(req,res)=>{
-        const result=await productCollection.estimatedDocumentCount();
-        res.send({totalProducts: result})
-    })
+    ///All Product Number
+    app.get("/totalproducts", async (req, res) => {
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({ totalProducts: result });
+    });
 
-    app.post('/productsbyid', async(req,res)=>{
-        const ids=req.body;
-        const objectIds=ids.map(id=>new ObjectId(id))
+    app.post("/productsbyid", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map((id) => new ObjectId(id));
 
-        const query ={ _id: {$in: objectIds }}
-        const result= await productCollection.find(query).toArray()
-       // console.log('products by ID: ',ids);
-        res.send(result)
-    })
-
+      const query = { _id: { $in: objectIds } };
+      const result = await productCollection.find(query).toArray();
+      // console.log('products by ID: ',ids);
+      res.send(result);
+    });
 
     ///Project work end
 
-
-
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 /////MongoDB work end
 
-
-
-app.listen(port,()=>{
-    console.log(`Ema-John server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Ema-John server is running on port: ${port}`);
+});
